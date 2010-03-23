1a303644699c74e0c5878634823dbbfd2db21b3a
Ben Hyde <bhyde@rosebud.cozy>
2010-03-23 19:54:57 -0400

    more test cases

--- tests.lisp
+++ tests.lisp
@@ -4,6 +4,10 @@
 
 ;; -*- Mode: Common-Lisp -*-
 
+(defun list-of-p (type list)
+  (and (listp list)
+       (loop for elm in list always (typep elm type))))
+
 (def-suite etsy-api :description "ETSY API")
 
 (in-suite etsy-api)
@@ -37,7 +41,69 @@
 
 (test get-user
   "Find the user bhyde based on his odd materials"
-  (is
-   (loop for user in (get-users-by-name "bhyde" :limit 100 :detail-level :high)
-      when (equal '("a_warm_zephyr") (slot-value user 'materials))
-      return user)))
+  (let ((users (get-users-by-name "bhyde" :limit 100 :detail-level :high)))
+    (is
+     (and (list-of-p 'user users)
+          (loop for user in users
+             when (equal '("a_warm_zephyr") (slot-value user 'materials))
+             return user)))))
+
+(test get-listings-by-color
+  (is (get-listings-by-color "pink" :limit 10 :detail-level :high)))
+
+(test get-gift-guides
+  (is (list-of-p 'gift-guide
+                 (get-gift-guides))))
+
+(test get-gift-guide-listings
+    (is (list-of-p 'listing
+                   (get-gift-guide-listings (slot-value (first (get-gift-guides)) 'guide-id)))))
+
+(test get-top-categories
+  (is (list-of-p 'string (get-top-categories))))
+
+(test get-child-categories
+  (is (list-of-p 'string (get-child-categories "art:drawing"))))
+
+
+
+(test get-top-tags
+  (is (list-of-p 'string (get-top-tags))))
+
+(test get-child-tags
+  (is (list-of-p 'string (get-child-tags "wood"))))
+
+(test get-listings-by-category
+  (is (list-of-p 'listing
+                 (get-listings-by-category "art:drawing:original_drawing"
+                                           :detail-level :high))))
+
+(test get-favorite-listings-of-user
+  (is (list-of-p 'listing
+                 (get-favorite-listings-of-user "bhyde"))))
+
+(test get-shop-listings
+  (is (list-of-p 'listing
+                 (get-shop-listings "mck254"))))
+  
+(test get-shops-by-name
+  (is (list-of-p 'shop
+                 (get-shops-by-name "doll"))))
+
+(test get-favorite-shops-of-user
+  (is (list-of-p 'shop
+                 (get-favorite-shops-of-user "mck254"))))
+
+(test get-featured-sellers
+  (is (list-of-p 'user
+                 (get-featured-sellers))))
+
+(test get-shop-details
+  (is (list-of-p 'shop
+                 (get-shop-details "mck254"))))
+
+
+                
+
+       
+       
